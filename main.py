945af5fa9c5ed864757f0338746946953d41bb8e
MadScotty <madscotty@gmail.com>
2017-10-18 02:15:41 -0500

    Cleaned up code

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-import simulator
+# Yahoo! Fantasy Sports Playoff Projector by MadScotty
+# WIP
+
 import pull_data
+import simulator
 import make_page
 
-# Adjust stuff here
+# Adjust constants here
 playoff_length = 3
 num_playoff_teams = 6
 simulations = 10000
 number_of_teams = 12
 
+# Grab data from Yahoo using Yahoo's API
 league_data = pull_data.pull_data(playoff_length)
 
+# Do the simulations
 playoff_data = simulator.season_simulator(league_data, num_playoff_teams, simulations)
 
-make_page.make_page(playoff_data, int(len(playoff_data) / 4))
+# Make a pretty html page with the results
+make_page.make_page(playoff_data, len(playoff_data))
